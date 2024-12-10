574f2f4223ddab1b550c108b60420a17bde9c0a5
aurbanow <aurbanow@pjwstk.edu.pl>
2024-12-10 12:31:18 +0100

    lab 11

---
+++ Westeros/westeros-data/src/main/java/com/westeros/data/repositories/CharacterRepository.java
@@ -0,0 +1,10 @@
+package com.westeros.data.repositories;
+
+import com.westeros.data.model.ActorRole;
+import com.westeros.data.model.CharacterRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CharacterRepository extends JpaRepository<ActorRole, Long> {
+}
